b00841a152592edff468bd4c3777190915cd1152
Lakshmiprasad <slakshmiprasad93@gmail.com>
2018-02-12 00:51:41 +0530

    feat: addrecipe basic

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { addRecipe } from '../actions';
 
 class App extends Component {
-  render () {
-    return <div>Hello World</div>
-  }
+	state = {
+		calendar: null
+	};
+
+	componentDidMount() {
+		const { store } = this.props;
+		console.log(store);
+		store.subscribe(() => {
+			this.setState(() => ({
+				calendar: store.getState()
+			}));
+		});
+	}
+
+	submitFood = () => {
+		this.props.store.dispatch(
+			addRecipe({
+				day: 'monday',
+				meal: 'breakfast',
+				recipe: {
+					label: this.input.value
+				}
+			})
+		);
+	};
+
+	render() {
+		return (
+			<div>
+				<input type="text" ref={input => (this.input = input)} placeholder="Monday's Breakfast" />
+				<button onClick={this.submitFood}>Submit</button>
+				<pre>
+					Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+				</pre>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
